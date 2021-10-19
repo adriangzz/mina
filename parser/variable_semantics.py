6235be3a60e3dc72b160e2b99976c9f7371aec5d
adriangzz <adriangonzalezmontemayor@gmail.com>
2021-10-19 18:44:18 -0500

    Update functiontable with new methods for adding variables

--- parser/variable_semantics.py
+++ parser/variable_semantics.py
@@ -12,30 +12,69 @@ class FunctionTable(object):
         self.funcNameMap = {}
         self.currFunc = ''
 
-    def addFunc(self, row) -> bool:
+    def addFunction(self, row: dict) -> bool:
+        '''
+        Checks name of function does not exist, then adds it to the dictionary using it's name as key.
+        Sets current function as this function.
+        Returns true if added, otherwise false.
+        '''
         if row['name'] not in self.funcNameMap:
             self.funcNameMap[row['name']] = row
             self.currFunc = row['name']
             return True
         return False
 
-    def getFunc(self, name) -> dict:
+    def addVariables(self, row: dict, name: str = None) -> bool:
+        '''
+        Function to add variables to current function.
+        Uses last added function as default, but an extra optional parameter with the name of the function is accepted.
+        Returns true if variables were added, false otherwise.
+        '''
+        if name and name in self.funcNameMap and row['name'] not in self.funcNameMap[name]['variables']:
+            self.funcNameMap[name]['variables'][row['name']] = row
+            return True
+        else:
+            if row['name'] not in self.funcNameMap[self.currFunc]['variables']:
+                self.funcNameMap[self.currFunc]['variables'][row['name']] = row
+                return True
+        return False
+
+    def getFunction(self, name: str) -> dict:
+        '''
+        Function that returns the dictionary of the name of function given.
+        Returns empty dict if name does not exist.
+        '''
         if name in self.funcNameMap:
             return self.funcNameMap[name]
         return {}
 
-    def deleteFunc(self, name) -> None:
+    def getFunctions(self) -> dict:
+        '''
+        Returns the whole dictionary of functions and variables.
+        '''
+        return self.funcNameMap
+
+    def deleteFunction(self, name: str) -> bool:
+        '''
+        Deletes function given the name.
+        Returns true if deleted, false otherwise.
+        '''
         if name in self.funcNameMap:
             self.funcNameMap.pop(name)
+            return True
+        return False
 
-    def funcExists(self, name) -> bool:
+    def functionExists(self, name: str) -> bool:
+        '''
+        Returns true if function exists in table, false otherwise.
+        '''
         return name in self.funcNameMap
 
 
 table = FunctionTable()
-print(table.addFunc({'name': 'a', 'type': 'int'}))
-print(table.addFunc({'name': 'a', 'type': 'int'}))
-print(table.getFunc('int'))
-print(table.funcExists('a'))
-print(table.deleteFunc('a'))
-print(table.funcExists('a'))
+table.addFunction({'name': 'adrian', 'variables': {}})
+table.addFunction({'name': 'david', 'variables': {}})
+table.addVariables({'name': 'a', 'type': 'int'})
+table.addVariables({'name': 'b', 'type': 'float'})
+print(table.addVariables({'name': 'a', 'type': 'string'}, "d"))
+print(table.getFunctions())
